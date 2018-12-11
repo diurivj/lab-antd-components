32e401307144542fb918ff4745d19aa431a2575c
sabasm <smma1992@gmail.com>
2018-12-10 20:34:38 -0600

    done

--- src/App.js
+++ src/App.js
@@ -11,26 +11,22 @@ class App extends Component {
 
       <h1>Sabás Mendívil</h1>
       <p>Fotografía</p>
-     
-  <Carousel autoplay>
-<div class="fotos">
+    
+  <Carousel vertical>
 
-</div>
+<div><img class="foto" src="https://res.cloudinary.com/sabasmendivil/image/upload/v1544491973/Fotos/43040452150_e692a6a8ff_z.jpg" alt="sabasfotografia"/></div>
+<div><img class="foto" src="https://res.cloudinary.com/sabasmendivil/image/upload/v1544491973/Fotos/43040496900_8bb9385a32_z.jpg" alt="sabasfotografia"/></div>
+<div><img class="foto" src="https://res.cloudinary.com/sabasmendivil/image/upload/v1544491973/Fotos/28366396749_a4e36059ae_z.jpg" alt="sabasfotografia"/></div>
+<div><img class="foto" src="https://res.cloudinary.com/sabasmendivil/image/upload/v1544491973/Fotos/36994260240_f4df7cea25_z.jpg" alt="sabasfotografia"/></div>
+<div><img class="foto" src="https://res.cloudinary.com/sabasmendivil/image/upload/v1544491973/Fotos/37280550420_cf95a24043_z.jpg" alt="sabasfotografia"/></div>
+<div><img class="foto" src="https://res.cloudinary.com/sabasmendivil/image/upload/v1544491973/Fotos/39229422245_f93ce294aa_z.jpg" alt="sabasfotografia"/></div>
+<div><img class="foto" src="https://res.cloudinary.com/sabasmendivil/image/upload/v1544491973/Fotos/28347972019_279518bb0e_z.jpg" alt="sabasfotografia"/></div>
+<div><img class="foto" src="https://res.cloudinary.com/sabasmendivil/image/upload/v1544491973/Fotos/42858039622_8c38c10d6d_z.jpg" alt="sabasfotografia"/></div>
   </Carousel>
 
-      <Button type="primary" onClick={()=>{ alert('smma1992@gmail.com'); }}>Contacto</Button>
-
+<div id="button"><Button type="primary" onClick={()=>{ alert('smma1992@gmail.com'); }}>Contacto</Button></div>
       </div>
     )
   }
 }
-const fotos = ['https://res.cloudinary.com/sabasmendivil/image/upload/v1544491973/Fotos/43040452150_e692a6a8ff_z.jpg',
-'https://res.cloudinary.com/sabasmendivil/image/upload/v1544491973/Fotos/43040496900_8bb9385a32_z.jpg',
-'https://res.cloudinary.com/sabasmendivil/image/upload/v1544491973/Fotos/28366396749_a4e36059ae_z.jpg',
-'https://res.cloudinary.com/sabasmendivil/image/upload/v1544491973/Fotos/36994260240_f4df7cea25_z.jpg',
-'https://res.cloudinary.com/sabasmendivil/image/upload/v1544491973/Fotos/37280550420_cf95a24043_z.jpg',
-'https://res.cloudinary.com/sabasmendivil/image/upload/v1544491973/Fotos/39229422245_f93ce294aa_z.jpg',
-'https://res.cloudinary.com/sabasmendivil/image/upload/v1544491973/Fotos/28347972019_279518bb0e_z.jpg',
-'https://res.cloudinary.com/sabasmendivil/image/upload/v1544491973/Fotos/42858039622_8c38c10d6d_z.jpg']
-
 export default App;
