bc27938fdf6a685dcb241af93b04d6f0781115a9
Pily <paila@live.com.mx>
2018-12-10 23:37:49 -0600

    intento1

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+import {Menu, Layout, Carousel}
+
+const {header, content, footer} = Layout
+
+const Cuerpo =()=>{
+  <div>
+    <Layout>
+       <Header>header</Header>
+       <Content>main content</Content>
+       <Footer>footer</Footer>
+   </Layout>
+  </div>
+}
