07f178db7e1142d4f8136ead48d3829b4ee53a60
Fernando Hernandez <fernandohernandez@MacBook-Pro-de-Fernando.local>
2018-12-10 18:48:32 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'antd'
+import { Carousel } from 'antd';
+import { Checkbox } from 'antd';
+import { Menu, Icon } from 'antd';
+import { DatePicker } from 'antd';
+
+
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
 
 class App extends Component {
+  
+  handleClick = (e) => {
+    console.log('click ', e);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="" style={{display: 'flex'}}>
+       <Menu
+        onClick={this.handleClick}
+        style={{ width: 256 }}
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        mode="inline"
+      >
+        <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Menu</span></span>}>
+          <MenuItemGroup key="g1" title="Puglandia">
+            <Menu.Item key="1">Pugs</Menu.Item>
+            <Menu.Item key="2">Dieta de Pugs</Menu.Item>
+          
+            <Menu.Item key="3">Cuidados</Menu.Item>
+            <Menu.Item key="4">Contacto</Menu.Item>
+          </MenuItemGroup>
+        </SubMenu>
+        
+      </Menu>
+        <Carousel autoplay >
+    <div><img src="https://www.perrospug.net/wp-content/uploads/2015/04/pug1.jpg"></img></div>
+    <div><img src="http://www.twitrcovers.com/wp-content/uploads/2014/02/Pet-Pugs-l.jpg"></img></div>
+    <div><img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/doug-the-pug-1516375006.jpg?crop=0.984xw:0.722xh;0,0.264xh&resize=1200:*"></img></div>
+   
+  </Carousel>
+ 
+  
+
+
       </div>
+      
     );
   }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Menu} from 'antd';
+
+const Header = () => (
+<Menu>
+
+  
+</Menu>
+
+)
+
+export default Header
